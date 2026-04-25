c44052f9bbb00a3da2a459ac7874c9d87e84ef88
Pratik <pratikgurung022@gmail.com>
2026-04-25 15:27:06 -0700

    Added some basic logic

---
+++ vars.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{   
+    std::cout << R"(In this tutorial we will talk about Basic var types in C++
+                A variable is a portion of memory to store a value 
+                An identifier is a way to write your variable names,
+                But you cannot have keywords as your created identifiers
+                Examples like: for, if, while, else, int, double, float, true, false, do, not, break etc.
+                )"
+            << "\n";
+
+    std::cout << "A variable is a portion of memory to store a value" << "\n";
+
+    return 0;
+}
