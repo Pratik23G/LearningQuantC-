4164bbf5462c667ce03322029919e075f696559b
Pratik Gurung <pratikgurung022@gmail.com>
2026-08-15 14:48:22 -0700

    Parse successfully detects | and extracts Message after the '|' symbol

--- basics/stringMasterGauntlet/logParser.cpp
+++ basics/stringMasterGauntlet/logParser.cpp
@@ -2,27 +2,36 @@
 #include <string>
 #include <string_view>
 
+constexpr int MAX_LEN { 200 };
+constexpr std::string_view SEPARATOR { "|" };
+
 std::string_view extractLevel(std::string_view line)
 {
-    pos = line.find('|');
-    length = line.substr(0, pos)
-    
+    auto pos { line.find(SEPARATOR) };
+    return line.substr(0, pos );    
 }
 
 std::string_view extractMessage(std::string_view line)
 {
-
+    auto pos { line.find(SEPARATOR) };
+    return line.substr(pos + 1);
 }
 
 
 int main()
 {
-    constexpr int MAX_LEN { 200 };
-    constexpr std::string_view SEPARATOR { "|" };
-    
-    auto level { extractLevel(std::string_view line) };
-    auto message { extractMessage(std::string_view line) };
+    std::string line;
+    std::getline(std::cin >> std::ws, line);
 
-    cout << "Level: " << level << "Message: " << message << "\n";
+    if(static_cast<int>(line.length()) > MAX_LEN)
+    {
+        std::cout << "The length of parsing is long please chunk it properly " << "\n";
+        return 1;
+    }
+
+    auto level { extractLevel(line) };
+    auto message { extractMessage(line) };
+
+    std::cout << "Level: " << level << " " << "Message: " << message << "\n";
     return 0;
 }
