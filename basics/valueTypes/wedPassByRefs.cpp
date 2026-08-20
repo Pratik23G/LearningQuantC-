2a2f87cb2ce027629e6e389dc690e520ac54a000
Pratik Gurung <pratikgurung022@gmail.com>
2026-08-20 09:15:13 -0700

    Pass by Reference can help to modify argument and its inexpensive than pass by value

---
+++ basics/valueTypes/wedPassByRefs.cpp
@@ -0,0 +1,74 @@
+/* 
+
+For todays practice I am learning about pass by reference and how it can be useful for
+tasks like std::string and other class types which when done through pass by reference is 
+a bit expensive.
+
+*/
+
+#include<iostream>
+#include<string>
+
+void printHello(std::string& x)
+{
+    std::cout << x << "\n";
+}
+
+void addressPrint(int val, int& ref)
+{
+    std::cout << "The address of value is: " << &val << "\n";
+    std::cout << "The address of reference is: " << &ref << "\n";
+}
+
+void printMessageVal(std::string userMessage, int countY)
+{
+    for(int i {}; i < countY; i++)
+    {
+        std::cout << userMessage << "\n";
+    }
+    countY = 0 ;
+}
+
+void printMessageRef(std::string& userMessage, int& countX)
+{
+    for(int i {}; i < countX; i++)
+    {
+        std::cout << userMessage << "\n";
+    }
+
+    countX = 0;
+}
+
+int main()
+{   
+
+    std::string x { "Hello World, Reference Magic " };
+
+    printHello(x);
+
+    //One cool thing about pass by reference is that, in
+    // value parameter is treated as a separate object than argument
+    // but in reference it is as if it were the argument so when 
+    // we pass by value the memory address is diff so ti make it identical
+    // we would have to make a copy into the memory which is expensive
+
+    int y { 6 };
+
+    std::cout << "The address of x value is: " << &y << "\n";
+
+    addressPrint(y, y);
+
+    /* 
+    
+    Pass by reference also helps us out to change the value of argument
+    while pass by value is a copy of its own so any changes made by 
+    value does no effect on argument, Here is an example with printing Hello world, how many times.
+    
+    */
+    std::string userResponse { "This is hello world message being Printed" };
+    int count { 2 };
+    printMessageVal(userResponse, count);
+    printMessageRef(userResponse, count);
+
+    return 0;
+}
